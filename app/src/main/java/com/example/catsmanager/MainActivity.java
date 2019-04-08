bc4fb9d6a6bb1731f6907914f709e3134e24ef65
13516127 Muhammad Azka Widyanto <13516127@std.stei.itb.ac.id>
2019-04-08 13:08:19 +0700

    merge from gitlab if3111/yeay

---
+++ app/src/main/java/com/example/catsmanager/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.catsmanager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<Cat> mCatsData;
+    private CatsAdapter mAdapter;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create an instance of the tab layout from the view
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        // Set the text for each tab
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+
+        // Set the tabs to fill the entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Using PagerAdapter to manager page views in fragments.
+        // Each page is represented by each own fragment
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        // Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        ImageButton signOutButton = (ImageButton) findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+               mAuth=FirebaseAuth.getInstance();
+               mAuth.signOut();
+               FirebaseUser user = mAuth.getCurrentUser();
+               updateUI(user);
+               }
+        });
+
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser==null) {
+            Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+            MainActivity.this.startActivity(myIntent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/catsmanager/TabFragment1.java
@@ -0,0 +1,77 @@
+package com.example.catsmanager;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.Context.SENSOR_SERVICE;
+import static android.support.v4.content.ContextCompat.getSystemService;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment1 extends Fragment {
+    private SensorManager sensorManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_fragment1, container, false);
+        final Spinner spinner = view.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.times, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        
+        
+        //handle spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                String text = parent.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        Button startButton = (Button) view.findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String timeValue =  spinner.getSelectedItem().toString();
+                Intent myIntent = new Intent(getActivity(), ProximityActivity.class);
+                myIntent.putExtra("timeValue", timeValue);
+                startActivity(myIntent);
+
+            }
+        });
+
+
+        Button checkButton = (Button) view.findViewById(R.id.button3);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getActivity(), LightSensor.class);
+                startActivity(myIntent);
+            }
+        });
+        return view;
+    }
+}
